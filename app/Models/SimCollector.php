292f83c34914fa7678bcd8e9203ddaaac04bb261
Malaosarl <contact@malaosarl.sn>
2026-04-16 11:01:36 +0000

    fix: supprimer HasApiTokens (Sanctum) de SimCollector + Carbon::parse profile

--- app/Models/SimCollector.php
+++ app/Models/SimCollector.php
@@ -5,11 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Laravel\Sanctum\HasApiTokens;
-
 class SimCollector extends Model
 {
-    use HasApiTokens;
     protected $fillable = [
         'name',
         'email',
