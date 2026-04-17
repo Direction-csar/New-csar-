ec041a8dfb2513cdfad901991d642f3616132005
Malaosarl <contact@malaosarl.sn>
2026-04-17 10:57:05 +0000

    fix: renommer PDF en avancement-tabaski-[nom].pdf

--- resources/views/public/tabaski/form.blade.php
+++ resources/views/public/tabaski/form.blade.php
@@ -380,7 +380,7 @@
 
         html2pdf().set({
             margin: 10,
-            filename: `ticket-tabaski-${nom.toLowerCase().replace(/\s+/g,'-')}.pdf`,
+            filename: `avancement-tabaski-${nom.toLowerCase().replace(/\s+/g,'-')}.pdf`,
             image: { type: 'jpeg', quality: 0.98 },
             html2canvas: { scale: 2, useCORS: true },
             jsPDF: { unit: 'mm', format: 'a5', orientation: 'portrait' }
