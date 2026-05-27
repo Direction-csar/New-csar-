cb79e59e6568421230e93db2b983092ff97b327e
Malaosarl <contact@malaosarl.sn>
2026-05-27 10:22:03 +0000

    fix: avoid PDO::MYSQL_ATTR_SSL_CA deprecation when SSL not configured

--- config/database.php
+++ config/database.php
@@ -58,9 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') && defined('PDO::MYSQL_ATTR_SSL_CA') ? array_filter([
+            'options' => (extension_loaded('pdo_mysql') && defined('PDO::MYSQL_ATTR_SSL_CA') && env('MYSQL_ATTR_SSL_CA')) ? [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            ] : [],
         ],
 
         'pgsql' => [
